4896fb3b6402bcd9b922b9cfaf3f882f0ee711cb
xN0ut <vera254d4@gmail.com>
2021-04-24 15:16:20 -0500

    Agregar archivos de proyecto.

---
+++ Blazor.Domain.Entities/Contribuyente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Domain.Entities
+{
+    public class Contribuyente
+    {
+        public string Tipo { get; set; }
+        public string Ruc { get; set; }
+        public string RazonSocial { get; set; }
+        public string NombreComercial { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+    }
+}
---
+++ WebSite/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
